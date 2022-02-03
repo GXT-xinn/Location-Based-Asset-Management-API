ef777fbd6078799bb1a030e2b03dfd2c34f96af6
Xintong Guan <xintong052499@gmail.com>
2022-02-03 01:39:40 +0000

    Initiated node environment

--- dataAPI.js
+++ dataAPI.js
@@ -1,12 +1,13 @@
-// express is the server that forms part of the nodejs program 
-var express = require('express'); 
-var path = require("path"); 
-var app = express(); 
-var fs = require('fs'); 
-// add an https server to serve files 
-var http = require('http'); 
-var httpServer = http.createServer(app); 
-httpsServer.listen(4480); 
-app.get('/',function (req,res) { 
- res.send("hello world from the Data API"); 
+// express is the server that forms part of the nodejs program
+var express = require('express');
+var path = require("path");
+var fs = require('fs');
+var http = require('http');
+var app = express();
+var httpServer = http.createServer(app);
+
+httpServer.listen(4480);
+
+app.get('/',function (req,res) {
+	res.send("Hello World from the class test server");
 });
